f133701e2e11a2f83b9a6d4e75c10ae62158ed35
Nathã <nathamelo2007@gmail.com>
2026-03-31 16:40:55 -0300

    feat: overloading de metodo

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/sobrecargametodos/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.sobrecargametodos.test;
+
+import academy.devdojo.maratonajava.javacore.sobrecargametodos.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+        anime.init("Hunter x Hunter", "TV", 148, "Ação");
+        anime.imprime();
+    }
+}
