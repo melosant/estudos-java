3a90f3673f9a9142e0692f207d82fbde23e29b52
Nathã <nathamelo2007@gmail.com>
2026-04-16 13:53:16 -0300

    feat: modificador static

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/domain/Carro.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/domain/Carro.java
@@ -3,18 +3,29 @@ package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.domain;
 public class Carro {
     private String nome;
     private double velocidadeMax;
-    public static double velocidadeLimite = 250; // pertence à classe, não a uma instância
+    private static double velocidadeLimite = 250; // pertence à classe, não a uma instância
 
     public void imprime(){
         System.out.println("-------------------");
         System.out.println("Nome : " + this.nome);
         System.out.println("Velocidade máxima : " + this.velocidadeMax);
         System.out.println("Velocidade limite : " + Carro.velocidadeLimite);
+        // o this se refere sempre ao objeto, logo um atributo static pertence à classe
     }
 
     public Carro(String nome, double velocidadeMax) {
         this.nome = nome;
         this.velocidadeMax = velocidadeMax;
+    }
+
+    // getters e setters static
+    // metodos staticos nao utilizam atributos de instancia, somente static (o contrario é válido)
+    public static void setVelocidadeLimite(double velocidadeLimite){
+        Carro.velocidadeLimite = velocidadeLimite;
+    }
+
+    public static double getVelocidadeLimite(){
+        return Carro.velocidadeLimite;
     }
 
     public String getNome() {
