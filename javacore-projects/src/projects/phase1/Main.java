6d52656391bc50fb85989643125762cdcf0b8c51
Nathã <nathamelo2007@gmail.com>
2026-04-30 11:39:07 -0300

    fix: correções para manter o código mais legível e validação nos construtores usando métodos set

--- javacore-projects/src/projects/phase1/Main.java
+++ javacore-projects/src/projects/phase1/Main.java
@@ -2,19 +2,22 @@ package projects.phase1;
 
 public class Main {
     public static void main(String[] args) {
-        Training treino1 = new Training(100);
+        Training treino1 = new Training(100, 8.3, "Crawl");
         Training treino2 = new Training(100, 12);
 
-        treino1.setStyle("Borboleta");
-        treino2.setStyle("Costas");
 
         treino1.showTraining();
         treino2.showTraining();
 
+
+        // Não irá alterar o tempo
         treino1.setTimeSec(-8);
         treino1.showTraining();
 
+
+        // Não irá alterar a distância
         treino2.setDistanceMeters(-90);
+        treino2.setStyle("Borboleta");
         treino2.showTraining();
     }
 }
