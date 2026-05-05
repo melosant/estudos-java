4553c3955085b990c668e8052c1df70365efa891
Nathã <nathamelo2007@gmail.com>
2026-05-05 12:34:02 -0300

    feat: herança pt1 - conceito

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Hheranca/test/HerancaTest01.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.javacore.Hheranca.test;
+
+import academy.devdojo.maratonajava.javacore.Hheranca.domain.Endereco;
+import academy.devdojo.maratonajava.javacore.Hheranca.domain.Funcionario;
+import academy.devdojo.maratonajava.javacore.Hheranca.domain.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua Sei lá");
+        endereco.setCep("111111-111");
+
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Thomaz");
+        pessoa.setCpf("111111111-11");
+        pessoa.setEndereco(endereco);
+        pessoa.imprime();
+
+        System.out.println("-------------------");
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Maria");
+        funcionario.setCpf("222222222-22");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(15000);
+        funcionario.imprime();
+
+    }
+}
