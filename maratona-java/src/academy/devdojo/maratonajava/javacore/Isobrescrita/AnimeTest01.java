ba340cecedbafe3afb32565588129ff8815ea5e7
Nathã <nathamelo2007@gmail.com>
2026-05-05 13:44:05 -0300

    feat: herança pt5 : override e toString

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Isobrescrita/AnimeTest01.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.javacore.Isobrescrita;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Hunter x Hunter");
+        System.out.println(anime);
+    }
+}
