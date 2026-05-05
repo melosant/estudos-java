205147da14bc489d6b20587a832b94fbe2a1c4fe
Nathã <nathamelo2007@gmail.com>
2026-05-05 12:50:20 -0300

    feat: herança pt2 - super

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Hheranca/domain/Funcionario.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Hheranca/domain/Funcionario.java
@@ -3,6 +3,12 @@ package academy.devdojo.maratonajava.javacore.Hheranca.domain;
 public class Funcionario extends Pessoa{
     private double salario;
 
+    public void imprime() {
+        // super faz referencia à superclasse, assim chamando o método imprime e sobrescevendo.
+        super.imprime();
+        System.out.println("Salário: R$" + this.salario);
+    }
+
     public double getSalario() {
         return salario;
     }
